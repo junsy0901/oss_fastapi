f785dd228db58f690aafde251f9a11a38591aaac
junsy <junsy0901@gmail.com>
2024-05-26 11:53:13 +0900

    change

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-import uvicorn
-
-app FastAPI()
-
-@app.get("/")
-async def welcome() -> dict:
-    return {
-        "msg" : "hello world?"
-    }
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+print('hello world')
